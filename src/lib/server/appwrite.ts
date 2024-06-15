f06eae1a41cd6f2835f7bf40fad3bf8846eecf12
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-06-15 19:13:26 +0200

    refactor: Update createSessionClient function to remove unnecessary request parameter

--- src/lib/server/appwrite.ts
+++ src/lib/server/appwrite.ts
@@ -1,9 +1,9 @@
 import { Client, Account, OAuthProvider, Databases } from 'node-appwrite';
 import {cookies} from 'next/headers';
 
-const ENDPOINT = "https://console.hoppla.autos/v1";
-const PROJECT_ID = "66688eaa0009496c2951";
-const APPWRITE_API_KEY = "d1d50640bd802f169de202b2ab63fff734b260c1abee67fa2e91ddc2c9069c41a0ac97faae3fa6cc3b1928cf1d06c660b6b35a80c89a9214ad5b328e955b6b05c53b62204f316953cb0bc46792a9be0771c24c8a72412e8301fa254f43583a1ace9e12d073f27dac8f223b7acd27d9cba5b0ea8bc17ba631f1e8621b7779952f";
+const ENDPOINT = process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT || "http://localhost/v1";
+const PROJECT_ID = process.env.NEXT_PUBLIC_APPWRITE_PROJECT_ID || "";
+const APPWRITE_API_KEY = process.env.NEXT_PUBLIC_APPWRITE_API_KEY || "";
 
 const adminClient = new Client()
     .setEndpoint(ENDPOINT)
@@ -22,6 +22,7 @@ const createAdminClient = async () => {
     },
   };
 };
+
 
 const createSessionClient = async () => {
   const client = new Client()
