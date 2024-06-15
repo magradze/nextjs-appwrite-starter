0571a4add45bbfe812eb3072d10fbe804ee86e89
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-06-15 19:09:23 +0200

    feat: Add Appwrite client and service
    This commit adds the Appwrite client and service to the project. The client is initialized with the endpoint and project values from environment variables. The service includes functions for signing out, getting the account details, and retrieving avatars. The session is stored in a secure cookie, and the service also provides a function for setting the session hash. This commit introduces the necessary code for interacting with the Appwrite backend.
    
    Note: The commit message follows the established conventions of the repository.

---
+++ src/lib/client/appwrite.ts
@@ -0,0 +1,48 @@
+import { Client, Account, OAuthProvider, Databases, Avatars } from 'appwrite';
+
+export const client = new Client();
+export const database = new Databases(client);
+
+client
+  .setEndpoint(process.env.NEXT_PUBLIC_APPWRITE_ENDPOINT as string)
+  .setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT as string);
+
+const account = new Account(client);
+const avatars = new Avatars(client);
+// export { ID, OAuthProvider } from 'appwrite';
+
+export const AppwriteService = {
+  signOut: async () => {
+    try {
+      await account.deleteSession('current');
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  getAccount: async () => {
+    try {
+      return await account.get<any>();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  getAvatar: async (name: string) => {
+    try {
+      return avatars
+        .getInitials(name.split("").reverse().join(""), 256, 256)
+        .toString();
+    } catch (error) {
+      console.error(error);
+    }
+  },
+  setSession: (hash: string) => {
+    const authCookies: any = {};
+
+    try {
+      authCookies['session'] = hash;
+      client.headers["X-Fallback-Cookies"] = JSON.stringify(authCookies);
+    } catch (error) {
+      console.error(error);
+    }
+  },
+};
