995291e17afb495c0b1aee91010205f899cf8455
Giorgi Magradze <giorgitmogveli@gmail.com>
2024-06-16 22:56:25 +0200

    refactor: Update account page to use Appwrite for user authentication
    The code changes in this commit update the account page to use Appwrite for user authentication. It replaces the previous implementation that used a custom API endpoint. This improves the security and reliability of the authentication process in the application.

--- src/app/(private)/account/page.tsx
+++ src/app/(private)/account/page.tsx
@@ -1,4 +1,26 @@
+"use client"
+import { Client, Account } from "appwrite";
+import { useEffect } from "react";
+
 const AccountPage = () => {
+
+  const client = new Client()
+    .setEndpoint("https://appwrite.hoppla.autos/v1")
+    .setProject("666de9da00196255c339")
+  const account = new Account(client);
+
+  const getLoggedInUser = async () => {
+    try {
+      return await account.get();
+    } catch (error) {
+      return null;
+    }
+  };
+
+  useEffect(() => {
+    getLoggedInUser();
+  }, []);
+
   return (
     <div>
       <h1>Account Page</h1>
